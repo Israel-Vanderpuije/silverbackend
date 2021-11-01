7026f37bcaeca6b1c05ed97d5027828c0b51e3ce
Israel Vanderpuije <vanderpuijeisrael10@gmail.com>
2021-11-01 05:43:14 +0000

    Initial commit

---
+++ src/main/java/com/silverback/silverbackend/SilverbackendApplication.java
@@ -0,0 +1,13 @@
+package com.silverback.silverbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverbackendApplication.class, args);
+	}
+
+}
